73836f7b74df076e844cfb3473bba0462c286903
iagofontes <iagofontes@hotmail.com>
2018-09-22 12:24:14 -0300

    inicio menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+// import {MatToolbar} from '@angular/material';
+import {MaterialModule} from '../components/material-module/material.module';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './../components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  exports: [
+    MaterialModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
---
+++ src/components/material-module/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import {MatToolbar, MatToolbarModule} from '@angular/material';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+      MatToolbarModule
+  ],
+  exports: [
+      MatToolbarModule
+  ]
+})
+export class MaterialModule { }
